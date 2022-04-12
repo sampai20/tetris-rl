c64e80b14779e1da31a261dca2e0ca24005dc3cf
sampai20 <sampai@mit.edu>
2022-04-12 05:03:57 -0400

    initial commit

---
+++ tetris-env/TODO.md
@@ -0,0 +1,11 @@
+Engine:
+- Scoring
+  + Add attack per line clear
+  + Combo scoring?
+- Garbage
+  + Implement garbage queue / API to add garbage
+- Better visualization
+  + pygame or mpl
+
+Environment:
+- do it lmao
---
+++ tetris-env/TetrisEngine.py
@@ -0,0 +1,367 @@
+import numpy as np
+
+class TetrisEngine():
+    '''
+    Python implementation of Tetris that is Guideline compliant (i.e. uses SRS rotation and Guideline piece spawning)
+    Kick tables / SRS algorithm taken from https://tetris.wiki/Super_Rotation_System
+    The action space consists of:
+    - 0 (1): DAS left (right), i.e. send piece maximally to the left (right).
+    - 2 (3): rotate left (right) using SRS rules.
+    - 4: soft drop, i.e. send piece maximally down.
+    - 5: hard drop, i.e. send piece maximally down and place.
+    - 6: hold piece, i.e. swap out piece with currently held piece.
+    '''
+    def __init__(self, board=None):
+        self._init_constants()
+        self.reset(board)
+
+    def _init_constants(self):
+        KICK_TABLE_DEFAULT = np.array([
+                [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],
+                [(0, 0), (-1, 0), (-1, -1), (0, 2), (-1, 2)],
+                [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],
+                [(0, 0), (1, 0), (1, -1), (0, 2), (1, 2)]
+        ])
+        KICK_TABLE_I = np.array([
+                [(0, 0), (-1, 0), (2, 0), (-1, 0), (2, 0)],
+                [(0, 1), (0, 1), (0, 1), (0, -1), (0, 2)],
+                [(-1, 1), (1, 1), (-2, 1), (1, 0), (-2, 0)],
+                [(-1, 0), (0, 0), (0, 0), (0, 1), (0, -2)]
+        ])
+        KICK_TABLE_O = np.array([
+                [(0, 0)],
+                [(-1, 0)],
+                [(-1, -1)],
+                [(0, -1)]
+        ])
+        self.PIECE_LETTERS = ['J', 'L', 'S', 'T', 'Z', 'I', 'O']
+        self.KICK_TABLES = [
+                KICK_TABLE_DEFAULT,
+                KICK_TABLE_DEFAULT,
+                KICK_TABLE_DEFAULT,
+                KICK_TABLE_DEFAULT,
+                KICK_TABLE_DEFAULT,
+                KICK_TABLE_I,
+                KICK_TABLE_O
+        ]
+        self.PIECE_OFFSETS = [
+                np.array([(0, 0), (-1, 0), (-1, 1), (1, 0)]),
+                np.array([(0, 0), (-1, 0), (1, 0), (1, 1)]),
+                np.array([(0, 0), (-1, 0), (0, 1), (1, 1)]),
+                np.array([(0, 0), (-1, 0), (0, 1), (1, 0)]),
+                np.array([(0, 0), (0, 1), (-1, 1), (1, 0)]),
+                np.array([(0, 0), (-1, 0), (1, 0), (2, 0)]),
+                np.array([(0, 0), (0, 1), (1, 0), (1, 1)])
+        ]
+        self.PIECE_COLORS = [
+                (0, 0, 255),
+                (255, 127, 0),
+                (0, 255, 0),
+                (127, 0, 127),
+                (255, 0, 0),
+                (0, 255, 255),
+                (255, 255, 0)
+        ]
+        self.COMBO_TABLE = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 4, 4, 5]
+        self.ATTACK_TABLE = [0, 0, 1, 2, 4]
+        self.T_SPIN_MULTIPLIER = 2
+        self.B2B_BONUS = 1
+        self.BOARD_WIDTH = 10
+        self.BOARD_HEIGHT = 20
+
+
+    def reset(self, board=None):
+        if board is None:
+            self.board = np.full((self.BOARD_WIDTH, self.BOARD_HEIGHT + 3), -1)
+        else:
+            self.board = board
+
+        # initialize piece
+        self.bags = (np.random.permutation(7), np.random.permutation(7)) # type: ignore
+        self.piece_index = 0
+        self.piece_pos = np.array([self.BOARD_WIDTH // 2 - 1, self.BOARD_HEIGHT - 1])
+        self.piece_rot = 0 # starts at original, increases CCW modulo 4
+
+        # held piece
+        self.held_piece = None
+        self.used_held = False
+        self.game_over = False
+
+        # attack
+        self.combo = 0
+        self.attack = 0
+        self.back_to_back = False
+
+        # t-spin tracking
+        self.last_rotate = False
+        self.last_kick = False
+
+        self.screen = None
+        self.clock = None
+
+    def _rotate(self, pos, rot):
+        dx, dy = pos[0], pos[1]
+        if rot == 0:
+            return pos
+        if rot == 1:
+            return np.array([-dy, dx])
+        if rot == 2:
+            return np.array([-dx, -dy])
+        if rot == 3:
+            return np.array([dy, -dx])
+        raise ValueError("rotation amount must be between 0 and 3 inclusive")
+    
+    def _rotate_offsets(self, piece, rot):
+        offsets = self.PIECE_OFFSETS[piece]
+        return np.array([self._rotate(offset, rot) for offset in offsets])
+
+    def _check_piece(self, squares):
+        if np.any(squares < 0) or np.any(squares >= [self.BOARD_WIDTH, self.BOARD_HEIGHT + 3]):
+            return False
+        return np.all(self.board[tuple(squares.T)] == -1)
+
+    def _piece_shift(self, delta):
+        offsets = self._rotate_offsets(self.bags[0][self.piece_index], self.piece_rot)
+        offsets += self.piece_pos + delta
+        if self._check_piece(offsets):
+            self.piece_pos += delta
+            self.last_rotate, self.last_kick = False, False
+            return True
+        return False
+        
+    def _max_piece_shift(self, delta):
+        offsets = self._rotate_offsets(self.bags[0][self.piece_index], self.piece_rot)
+        offsets += self.piece_pos
+        num_shifts = 0
+        while True:
+            offsets += delta
+            if self._check_piece(offsets):
+                num_shifts += 1
+            else:
+                break
+
+        self.piece_pos += num_shifts * delta
+        if num_shifts:
+            self.last_rotate, self.last_kick = False, False
+        return num_shifts
+
+    def _rotate_piece(self, rot):
+        piece = self.bags[0][self.piece_index]
+        cur_kick = self.KICK_TABLES[piece][self.piece_rot]
+        next_kick = self.KICK_TABLES[piece][(self.piece_rot + rot) % 4]
+        kick_deltas = cur_kick - next_kick
+        offsets = self._rotate_offsets(piece, (self.piece_rot + rot) % 4)
+        for i in range(kick_deltas.shape[0]):
+            if self._check_piece(offsets + kick_deltas[i] + self.piece_pos):
+                self.piece_pos += kick_deltas[i]
+                self.piece_rot = (self.piece_rot + rot) % 4
+                self.last_rotate = True
+                if i == 4:
+                    self.last_kick = True
+                return True
+        return False
+
+    def _new_piece(self):
+        self.piece_index += 1
+        if self.piece_index == 7:
+            self.bags = (self.bags[1], np.random.permutation(7)) # type: ignore
+            self.piece_index = 0
+        self.piece_rot = 0
+        self.piece_pos = np.array([self.BOARD_WIDTH // 2 - 1, self.BOARD_HEIGHT - 1])
+        self.used_held = False
+        self.last_rotate, self.last_kick = False, False
+
+        squares = self.PIECE_OFFSETS[self.bags[0][self.piece_index]] + self.piece_pos # type: ignore
+        if not self._check_piece(squares):
+            self.game_over = True
+
+    def _clear_lines(self):
+        line_mask = np.any(self.board == -1, axis=0)
+        num_cleared = self.board.shape[1] - line_mask.sum()
+        if num_cleared:
+            new_board = np.pad(self.board[:, line_mask], ((0, 0), (0, num_cleared)), 'constant', constant_values=(0, -1))
+            self.board = new_board
+        return num_cleared
+
+    def _hard_drop(self):
+        self._max_piece_shift(np.array([0, -1]))
+        squares = self._rotate_offsets(self.bags[0][self.piece_index], self.piece_rot) + self.piece_pos
+        self.board[tuple(squares.T)] = self.bags[0][self.piece_index]
+
+        # check for t-spin
+        tspin = False
+        if self.PIECE_LETTERS[self.bags[0][self.piece_index]] == 'T':
+            corners = [[1, 1], [-1, 1], [-1, -1], [1, -1]]
+            proper, total = 0, 0
+            for i in range(4):
+                square = self.piece_pos + corners[i]
+                if np.any(square < [0, 0]) or square[0] >= self.BOARD_WIDTH or self.board[tuple(square)] != -1:
+                    total += 1
+                    if i == self.piece_rot or i == (self.piece_rot + 1) % 4:
+                        proper += 1
+            tspin = self.last_rotate and ((total >= 3 and proper == 2) or (total >= 3 and self.last_kick))
+
+        num_clear = self._clear_lines()
+        reward = 0
+
+        if num_clear == 0:
+            self.combo = 0
+        else:
+            if tspin:
+                reward += 2 * num_clear
+            else:
+                reward += self.ATTACK_TABLE[num_clear] # type: ignore
+
+            reward += self.COMBO_TABLE[min(self.combo, len(self.COMBO_TABLE))]
+            self.combo += 1
+            
+            if tspin or num_clear == 4:
+                if self.back_to_back:
+                    self.attack += 1
+                self.back_to_back = True
+            else:
+                self.back_to_back = False
+
+        self._new_piece()
+
+        self.attack += reward
+        return reward
+
+    def _hold_piece(self):
+        if self.used_held:
+            return False
+        if self.held_piece is None:
+            self.held_piece = self.bags[0][self.piece_index]
+            self._new_piece()
+        else:
+            self.held_piece, self.bags[0][self.piece_index] = self.bags[0][self.piece_index], self.held_piece
+            self.piece_index -= 1
+            self._new_piece()
+        self.used_held = True
+        return True
+
+    def get_previews(self, num_previews=5):
+        if self.piece_index + num_previews > 6:
+            return np.concatenate((self.bags[0][self.piece_index + 1:7], self.bags[1][:num_previews + self.piece_index  - 6])) # type: ignore
+        else:
+            return self.bags[0][self.piece_index + 1: self.piece_index + num_previews + 1]
+
+    def handle_action(self, action_id):
+        '''
+        - 0 (1): DAS left (right), i.e. send piece maximally to the left (right).
+        - 2 (3): tap left (right), i.e. move once left (right) if possible.
+        - 4 (5): rotate left (right) using SRS rules.
+        - 6: soft drop, i.e. send piece maximally down.
+        - 7: hard drop, i.e. send piece maximally down and place.
+        - 8: hold piece, i.e. swap out piece with currently held piece.
+        '''
+        reward = 0
+
+        if action_id == 0:
+            self._max_piece_shift(np.array([-1, 0]))
+        elif action_id == 1:
+            self._max_piece_shift(np.array([1, 0]))
+        elif action_id == 2:
+            self._piece_shift(np.array([-1, 0]))
+        elif action_id == 3:
+            self._piece_shift(np.array([1, 0]))
+        elif action_id == 4:
+            self._rotate_piece(1)
+        elif action_id == 5:
+            self._rotate_piece(3)
+        elif action_id == 6:
+            self._max_piece_shift(np.array([0, -1]))
+        elif action_id == 7:
+            reward = self._hard_drop()
+        elif action_id == 8:
+            self._hold_piece()
+
+        return reward
+
+    def vis_board(self):
+        squares = self._rotate_offsets(self.bags[0][self.piece_index], self.piece_rot) + self.piece_pos
+        self.board[tuple(squares.T)] = self.bags[0][self.piece_index]
+        board_str = "#" * (self.BOARD_WIDTH + 2) + "\n#"
+        for i in range(self.BOARD_HEIGHT, -1, -1):
+            for j in range(self.BOARD_WIDTH):
+                square = self.board[j, i]
+                board_str += (self.PIECE_LETTERS[square] if square != -1 else " ")
+            board_str += "#\n#"
+        board_str += "#" * (self.BOARD_WIDTH + 1)
+        board_str += "\nHeld: {0}".format(self.PIECE_LETTERS[self.held_piece] if self.held_piece else None)
+        board_str += "\nPreviews: {0}".format(" ".join(map(lambda x : self.PIECE_LETTERS[x], self.get_previews())))
+        board_str += "\nAttack: {0}".format(self.attack)
+        self.board[tuple(squares.T)] = -1
+        return board_str
+
+    def render(self):
+        # modified from https://github.com/openai/gym/blob/master/gym/envs/classic_control/cartpole.py
+        import pygame
+        from pygame import gfxdraw
+
+        screen_width, screen_height = 400, 800
+        side = 20
+        margin = 10
+
+        if self.screen is None:
+            pygame.init()
+            pygame.display.init()
+            self.screen = pygame.display.set_mode((screen_width, screen_height))
+        if self.clock is None:
+            self.clock = pygame.time.Clock()
+
+        self.surf = pygame.Surface((screen_width, screen_height))
+        self.surf.fill((255, 255, 255))
+        
+        # draw box
+        game_area = pygame.Rect(margin, margin, self.BOARD_WIDTH * side, self.BOARD_HEIGHT * side)
+        gfxdraw.rectangle(self.surf, game_area, (0, 0, 0))
+
+        # draw board
+        squares = self._rotate_offsets(self.bags[0][self.piece_index], self.piece_rot) + self.piece_pos
+        self.board[tuple(squares.T)] = self.bags[0][self.piece_index]
+        for i in range(self.BOARD_WIDTH):
+            for j in range(self.BOARD_HEIGHT):
+                square = self.board[i, j]
+                if square != -1:
+                    box = pygame.Rect((margin + i * side + 1, margin + j * side - 1, side-1, side-1))
+                    color = pygame.Color(self.PIECE_COLORS[square])
+                    gfxdraw.box(self.surf, box, color) 
+        self.board[tuple(squares.T)] = -1
+
+
+        self.surf = pygame.transform.flip(self.surf, False, True)
+        self.screen.blit(self.surf, (0, 0))
+
+        pygame.event.pump()
+        self.clock.tick(50)
+        pygame.display.flip()
+
+
+
+if __name__ == '__main__':
+    import time
+
+    board = np.array([
+        [1, 1, 1, 1, 1, 1, 1, 1, -1, 1],
+        [1, 1, 1, 1, 1, 1, 1, -1, -1, -1],
+        [1, 1, 1, 1, 1, 1, 1, 1, -1, -1]
+    ]).T
+    board = np.pad(board, ((0, 0), (0, 23 - board.shape[1])), 'constant', constant_values=(-1, -1))
+    engine = TetrisEngine(board)
+    while not engine.game_over:
+        engine.render()
+        # print(engine.vis_board())
+        a = np.random.randint(9) # type: ignore
+        try:
+            engine.handle_action(int(a))
+        except Exception as e:
+            print(e)
+        time.sleep(0.01)
+
+
+
+
+
+
+
